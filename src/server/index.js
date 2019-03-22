16df9209d6829f82e0d2d33deb52ddb226c2da64
Lei Nelissen <lei@codified.nl>
2019-03-22 14:52:05 +0100

    Add datalayer via CouchDB

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,10 @@
+require('dotenv').config()
 const WebSocket = require('ws');
+const http = require('http');
+const serveStatic = require('serve-static');
+var finalhandler = require('finalhandler');
+const nano = require('nano')(process.env.COUCHDB_URL);
+const db = nano.db.use(process.env.COUCHDB_DB);
 
 // Setup base WebSockets Server
 const wss = new WebSocket.Server({ port: 3001 });
@@ -8,6 +14,7 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log(`[${new Date().toISOString()}] Received: ${message}`);
         ws.send('ACK');
+        db.insert(JSON.parse(message));
     });
     console.log(ws._socket.remoteAddress);
 });
@@ -16,5 +23,16 @@ wss.on('connection', function connection(ws) {
 wss.on('error', console.error);
 
 // Notify CLI that the server has started
-wss.on('listening', () => console.log('Server has started...'));
+wss.on('listening', () => console.log('Websockets server has started...'));
 
+// Create static server
+const serve = serveStatic('./public');
+
+// Define node HTTP server
+var server = http.createServer(function onRequest (req, res) {
+    serve(req, res, finalhandler(req, res));
+});
+
+// Make HTTP server listen on supplied port
+server.listen(process.env.HTTP_PORT);
+console.log('HTTP server has started...');
